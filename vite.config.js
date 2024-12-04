cc21f3bdd84c0025673bcfb749c7a864de0db30e
Rovak0 <ericjackwong@outlook.com>
2024-12-04 15:38:48 -0500

    testing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/',
+  // base: '/vite-deploy-demo/',
   server: {
     port: 3000,
     open: true,
