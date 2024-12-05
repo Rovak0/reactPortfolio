11343b137696b0df503b84fb559333b1303d64e1
Rovak0 <ericjackwong@outlook.com>
2024-12-05 12:33:52 -0500

    declared port

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+//add a PORT variable to manage deployment
+const PORT = process.env.PORT || 3001;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // base: '/vite-deploy-demo/',
   server: {
-    port: 3000,
+    port: PORT,
     open: true,
   },
 });
